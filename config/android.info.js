4182bc289337cce6ddf6992639701843f584f951
Pallabi Bhowmick <pallabiofficial9@gmail.com>
2026-07-19 01:02:42 +0100

    WebdriverIO-V6-appium-cucumber-boilerplate-master

---
+++ config/android.info.js
@@ -0,0 +1,15 @@
+class AndroidInfo {
+    static deviceName() {
+        return '5527c8fb'; // pass the udid or devicename
+    }
+
+    static platFormVersion() {
+        return '9'; // pass the platform version
+    }
+
+    static appName() {
+        return 'Celsius Fahrenheit Converter_v1.0.1_apkpure.com.apk'; // pass the apk name
+    }
+}
+
+module.exports = AndroidInfo;
